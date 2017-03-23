8a64a3bb2a298948e9fdc29430571575949d9128
Adrian Kelly <hello@adriankelly.net>
2017-03-23 14:21:00 +0100

    Create basic case list and case detail view

--- reporter/urls.py
+++ reporter/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^cases/$', views.CaseListView.as_view(), name='cases'),
+    url(r'^case/(?P<pk>\d+)/$', views.CaseDetailView.as_view(), name='case-detail'),
+
 ]
