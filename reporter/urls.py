e30000198a2fd3519fc5bcbb8a94e5ee10f9fba1
Adrian Kelly <hello@adriankelly.net>
2017-03-23 14:36:05 +0100

    Create basic author views

--- reporter/urls.py
+++ reporter/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^cases/$', views.CaseListView.as_view(), name='cases'),
     url(r'^case/(?P<pk>\d+)/$', views.CaseDetailView.as_view(), name='case-detail'),
-
+    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    url(r'^author/(?P<pk>\d+)/$', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
