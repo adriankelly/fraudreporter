7bc437a5127d71eee62f4d5c8109c18ec188963a
Adrian Kelly <hello@adriankelly.net>
2017-03-23 13:39:42 +0100

    Creation of admin account

--- reporter/admin.py
+++ reporter/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Case, Report, Author
+
+admin.site.register(Case)
+admin.site.register(Report)
+admin.site.register(Author)
