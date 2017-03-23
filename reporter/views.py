a57e097e823aa4cc5a6c702c9fd562980255ba9a
Adrian Kelly <hello@adriankelly.net>
2017-03-23 15:15:58 +0100

    Create basic login auth for pages

--- reporter/views.py
+++ reporter/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Case, Report, Author
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
@@ -25,17 +26,17 @@ def index(request):
         context={'num_cases':num_cases,'num_reports':num_reports,'num_cases_unresolved':num_cases_unresolved, 'num_authors':num_authors, 'num_visits':num_visits},
     )
 
-class CaseListView(generic.ListView):
+class CaseListView(LoginRequiredMixin, generic.ListView):
     model = Case
     paginate_by = 10
 
-class CaseDetailView(generic.DetailView):
+class CaseDetailView(LoginRequiredMixin, generic.DetailView):
     model = Case
 
 
-class AuthorListView(generic.ListView):
+class AuthorListView(LoginRequiredMixin, generic.ListView):
     model = Author
     paginate_by = 10
 
-class AuthorDetailView(generic.DetailView):
+class AuthorDetailView(LoginRequiredMixin, generic.DetailView):
     model = Author
